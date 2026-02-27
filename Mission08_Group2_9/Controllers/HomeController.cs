c3e862d685051a1fb76160651c02c52d9cbc3c35
mhansen71 <hmitch16@byu.edu>
2026-02-27 15:32:57 -0700

    Add views, update controller and repository

--- Mission08_Group2_9/Controllers/HomeController.cs
+++ Mission08_Group2_9/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Mission08_Group2_9.Models;
 
 namespace Mission08_Group2_9.Controllers;
@@ -12,14 +13,59 @@ public class HomeController : Controller
     {
         _repo = temp;
     }
+
     public IActionResult Index()
     {
-        return View();
+        var tasks = _repo.Tasks.Where(t => t.Completed != true).ToList();
+        return View(tasks);
     }
 
-    public IActionResult Privacy()
+    [HttpGet]
+    public IActionResult AddEditTask(int? id)
     {
-        return View();
+        ViewBag.Categories = new SelectList(_repo.Categories, "CategoryId", "CategoryName");
+        
+        if (id == null)
+            return View(new TaskItem());
+        
+        var task = _repo.Tasks.FirstOrDefault(t => t.TaskId == id);
+        return View(task);
+    }
+
+    [HttpPost]
+    public IActionResult AddEditTask(TaskItem taskItem)
+    {
+        if (ModelState.IsValid)
+        {
+            if (taskItem.TaskId == 0)
+                _repo.AddTask(taskItem);
+            else
+                _repo.UpdateTask(taskItem);
+        
+            return RedirectToAction("Index");
+        }
+    
+        ViewBag.Categories = new SelectList(_repo.Categories, "CategoryId", "CategoryName");
+        return View(taskItem);
+    }
+
+    [HttpPost]
+    public IActionResult DeleteTask(int taskId)
+    {
+        _repo.DeleteTask(taskId);
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult MarkComplete(int taskId)
+    {
+        var task = _repo.Tasks.FirstOrDefault(t => t.TaskId == taskId);
+        if (task != null)
+        {
+            task.Completed = true;
+            _repo.UpdateTask(task);
+        }
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Mission08_Group2_9/Models/ITaskRepository.cs
+++ Mission08_Group2_9/Models/ITaskRepository.cs
@@ -3,4 +3,8 @@ namespace Mission08_Group2_9.Models;
 public interface ITaskRepository
 {
     List<TaskItem> Tasks { get; }
+    List<Category> Categories { get; }
+    void AddTask(TaskItem task);
+    void UpdateTask(TaskItem task);
+    void DeleteTask(int taskId);
 }
